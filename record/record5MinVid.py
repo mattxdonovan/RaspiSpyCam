661d9210a91965506b65e7b4bfbc89436e952894
mattxdonovan <mattxd12@gmail.com>
2017-02-01 19:28:44 -0700

    unique file writing added

---
+++ record/record5MinVid.py
@@ -0,0 +1,12 @@
+import time
+import os
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN)
+
+while True:
+    if GPIO.input(23)==1:
+        print ("Taking Video")
+        timestr = time.strftime('%Y%m%d-%H%M%S')
+        os.system('raspivid -o /home/pi/Desktop/' + timestr + '.h264 -t 300000')
