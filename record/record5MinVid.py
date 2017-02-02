13c740e3de0d50c02aef65eb2dfa0adf884e5ee6
mattxdonovan <mattxd12@gmail.com>
2017-02-01 19:29:28 -0700

    finished script

--- record/record5MinVid.py
+++ record/record5MinVid.py
@@ -10,3 +10,8 @@ while True:
         print ("Taking Video")
         timestr = time.strftime('%Y%m%d-%H%M%S')
         os.system('raspivid -o /home/pi/Desktop/' + timestr + '.h264 -t 300000')
+        print ('Finished Taking Video')
+        os.system('killall raspivid')
+    else:
+        print ('')
+GPIO.cleanup()
